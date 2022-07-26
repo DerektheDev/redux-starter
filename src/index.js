5557ded26057ff11763e44d5fc878f32cc5f845e
Derek Montgomery <hello@derekthe.dev>
2022-07-26 10:27:15 -0500

    complete section 3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+import store from "./store";
+
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
+
+// const unsubscribe = store.subscribe(() => {
+//   console.log("Store changed!", store.getState());
+// });
+
+store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugResolved(1));
+
+// unsubscribe();
+
+// store.dispatch({
+//   type: actions.BUG_REMOVED,
+//   payload: {
+//     id: 1,
+//   },
+// });
+
+console.log(store.getState());
