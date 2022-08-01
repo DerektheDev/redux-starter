f27dc7487b4393e0b3da972135a891b93011353c
Derek Montgomery <hello@derekthe.dev>
2022-07-31 21:13:48 -0500

    finish 5.7

--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ import * as actions from "./bugs/actions";
 
 const store = configureStore();
 
-store.dispatch(actions.bugAdded("Bug 1"));
-store.dispatch(actions.bugAdded("Bug 2"));
-store.dispatch(actions.bugAdded("Bug 3"));
-store.dispatch(actions.bugResolved(1));
+store.dispatch(actions.bugAdded({ description: "Bug 1" }));
+store.dispatch(actions.bugAdded({ description: "Bug 2" }));
+store.dispatch(actions.bugAdded({ description: "Bug 3" }));
+store.dispatch(actions.bugResolved({ id: 1 }));
 
 // unsubscribe();
 
