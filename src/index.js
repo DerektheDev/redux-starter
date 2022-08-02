41f01be2739b7cf6b90f67883f9f5112cbd13b65
Derek Montgomery <hello@derekthe.dev>
2022-08-02 17:36:20 -0500

    finish 8.4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import configureStore from "./store/configureStore";
 import {
-  bugAdded,
-  bugResolved,
-  getUnresolvedBugs,
-  bugAssignedToUser,
+  // bugAdded,
+  // bugResolved,
+  // getUnresolvedBugs,
+  // bugAssignedToUser,
   getBugsByUser,
 } from "./store/bugs";
 import { projectAdded } from "./store/projects";
@@ -12,13 +12,13 @@ import { userAdded } from "./store/users";
 const store = configureStore();
 
 store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
-store.dispatch(projectAdded({ name: "Project 1" }));
-store.dispatch(bugAdded({ name: "Bug 1" }));
-store.dispatch(bugAdded({ name: "Bug 2" }));
-store.dispatch(bugAdded({ name: "Bug 3" }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(userAdded({ name: "User 2" }));
+// store.dispatch(projectAdded({ name: "Project 1" }));
+// store.dispatch(bugAdded({ name: "Bug 1" }));
+// store.dispatch(bugAdded({ name: "Bug 2" }));
+// store.dispatch(bugAdded({ name: "Bug 3" }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
+// store.dispatch(bugResolved({ id: 1 }));
 
 // const unresolvedBugs = getUnresolvedBugs(store.getState());
 // console.log(unresolvedBugs);
